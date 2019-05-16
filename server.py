63becd7e49567d738ff7ad38725c4f561d746f9b
John Mitchell <jmitchell@tuition.io>
2019-05-16 16:23:55 -0700

    test wip set/get

--- server.py
+++ server.py
@@ -61,6 +61,7 @@ def get(key):
 
 @app.route('/set/<key>', methods=['POST'])
 def set(key):
+    import ipdb; ipdb.set_trace()
     try:
         payload = request.get_json(force=True)
         if 'value' in payload:
--- test_functional.py
+++ test_functional.py
@@ -1,3 +1,8 @@
+import random
+
+def make_random():
+    return str(random.randint(10000, 99999))
+
 def test_health(test_client):
     """
     GIVEN a Flask application
@@ -7,3 +12,18 @@ def test_health(test_client):
     response = test_client.get('/')
     assert response.status_code == 200
     assert b'{"redis":true,"status":"up"}\n' == response.data
+
+# TODO reset database after each test
+def test_setget(test_client):
+    key = make_random()
+    value = make_random()
+
+    response = test_client.get(f'/get/{key}')
+    assert response.status_code == 404
+
+    response = test_client.post(f'/set/{key}', json={'value': value})
+    assert response.status_code == 200
+
+    response = test_client.get(f'/get/{key}')
+    assert 0, response
+    assert response.status_code == 404
